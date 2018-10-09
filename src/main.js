d26f3126b2c84d5040bbfb747de10d52aa0329ff
smilewjl1 <1351302735@qq.com>
2018-10-09 16:43:59 +0800

    项目首页布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入路由插件
+import VueRouter from 'vue-router'
+
+import index from './components/index.vue';
+//use一下
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  {
+    path:'/',
+    component:index
+  },
+  {
+    path:'/index',
+    component:index
+  }
+];
+
+//实例化路由
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
